d7952a2fe4a581ec806c8ab985daf4c0e75ba2ac
Rolucatelli <rolucatelli@gmail.com>
2025-03-25 15:01:05 -0300

    Implementação do filtro de Mediana

--- Aula 11-03/ProcImgC/ProcImg/codigoBase/base.c
+++ Aula 11-03/ProcImgC/ProcImg/codigoBase/base.c
@@ -78,7 +78,7 @@ image sobel(image img)
         {
             int soma, Z[9];
             int k = 0;
-            //Calculando a mascara que vai ser aplicada ao pixel
+            // Calculando a mascara que vai ser aplicada ao pixel
             for (int y = -1; y < 2; y++)
                 for (int x = -1; x < 2; x++)
                 {
@@ -86,8 +86,70 @@ image sobel(image img)
                 }
             soma = abs((Z[6] + 2 * Z[7] + Z[8]) - (Z[0] + 2 * Z[1] + Z[2])) +
                    abs((Z[2] + 2 * Z[5] + Z[8]) - (Z[0] + 2 * Z[3] + Z[6]));
-            //Aplicando o resultado a imagem de saida
+            // Aplicando o resultado a imagem de saida
             out->px[i * w + j] = soma > 168 ? 0 : max;
+        }
+    return out;
+}
+
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+void sort(int v[], int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        int m = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (v[j] < v[m])
+            {
+                m = j;
+            }
+            
+        }
+        int tmp = v[m];
+        v[m] = v[i];
+        v[i] = tmp;
+    }    
+}
+
+image mediana(image img)
+{
+    image out = img_clone(img);
+    int w = img->nc;
+    int h = img->nr;
+    int max = img->ml;
+    int *pixels = img->px;
+
+    // Inicializando a primeira e a ultima coluna da imagem de saida
+    for (int i = 0; i < h; i++)
+    {
+        out->px[i * w] = 0;
+        out->px[i * w + w - 1] = 0;
+    }
+    // Inicializando a primeira e a ultima linha da imagem de saida
+    for (int j = 0; j < w; j++)
+    {
+        out->px[j] = 0;
+        out->px[(h - 1) * w + j] = 0;
+    }
+
+    for (int i = 1; i < h - 1; i++)
+        for (int j = 0; j < w - 1; j++)
+        {
+            int Z[9];
+            int k = 0;
+            // Calculando a mascara que vai ser aplicada ao pixel
+            for (int y = -1; y < 2; y++)
+                for (int x = -1; x < 2; x++)
+                {
+                    Z[k++] = pixels[(i + y) * w + (j + x)];
+                }
+            // Aplicando o resultado a imagem de saida
+            qsort(Z,9,sizeof(int),compare);
+            // sort(Z,9);
+            out->px[i * w + j] = Z[4];
         }
     return out;
 }
@@ -118,8 +180,12 @@ int main(int argc, char *argv[])
     //-- read image
     In = img_get(nameIn, GRAY);
     //-- transformation
+
     // Out = eq(In);
-    Out = sobel(In);
+    // Out = sobel(In);
+    Out = mediana(In);
+
+
     //-- save image
     img_put(Out, nameOut, GRAY);
 
