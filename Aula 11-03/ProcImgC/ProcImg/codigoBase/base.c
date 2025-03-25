86e7f46c425f7253894cf4aaaa7bc13efb9a05a7
Rolucatelli <rolucatelli@gmail.com>
2025-03-25 14:37:47 -0300

    Implementação da função de realçe de bordas da imagem

--- Aula 11-03/ProcImgC/ProcImg/codigoBase/base.c
+++ Aula 11-03/ProcImgC/ProcImg/codigoBase/base.c
@@ -47,9 +47,54 @@ image eq(image img)
     return img;
 }
 
+int abs(int n)
+{
+    return n < 0 ? -n : n;
+}
+
+image sobel(image img)
+{
+    image out = img_clone(img);
+    int w = img->nc;
+    int h = img->nr;
+    int max = img->ml;
+    int *pixels = img->px;
+
+    // Inicializando a primeira e a ultima coluna da imagem de saida
+    for (int i = 0; i < h; i++)
+    {
+        out->px[i * w] = 0;
+        out->px[i * w + w - 1] = 0;
+    }
+    // Inicializando a primeira e a ultima linha da imagem de saida
+    for (int j = 0; j < w; j++)
+    {
+        out->px[j] = 0;
+        out->px[(h - 1) * w + j] = 0;
+    }
+
+    for (int i = 1; i < h - 1; i++)
+        for (int j = 0; j < w - 1; j++)
+        {
+            int soma, Z[9];
+            int k = 0;
+            //Calculando a mascara que vai ser aplicada ao pixel
+            for (int y = -1; y < 2; y++)
+                for (int x = -1; x < 2; x++)
+                {
+                    Z[k++] = pixels[(i + y) * w + (j + x)];
+                }
+            soma = abs((Z[6] + 2 * Z[7] + Z[8]) - (Z[0] + 2 * Z[1] + Z[2])) +
+                   abs((Z[2] + 2 * Z[5] + Z[8]) - (Z[0] + 2 * Z[3] + Z[6]));
+            //Aplicando o resultado a imagem de saida
+            out->px[i * w + j] = soma > 168 ? 0 : max;
+        }
+    return out;
+}
+
 void msg(char *s)
 {
-    printf("\nEqualizacao de histograma");
+    printf("\n Filters");
     printf("\n-------------------------------");
     printf("\nUsage:  %s  image-name[.pgm]\n\n", s);
     printf("    image-name[.pgm] is image file in pgm format \n\n");
@@ -73,7 +118,8 @@ int main(int argc, char *argv[])
     //-- read image
     In = img_get(nameIn, GRAY);
     //-- transformation
-    Out = eq(In);
+    // Out = eq(In);
+    Out = sobel(In);
     //-- save image
     img_put(Out, nameOut, GRAY);
 
